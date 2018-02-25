04cfa9e492f329ac67770ead964577422244673e
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-02-25 10:37:46 +0100

    done 13

--- src/ex13.java
+++ src/ex13.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 
 public class ex13 {
     public static void main(String[] args) {
-        BankAccount account1 = new BankAccount("casper",500,10);
+        BankAccount account1 = new BankAccount("casper",500,0);
+        BankAccount account2 = new BankAccount("freja", 200,0);
 
 
-        //account1.withdraw(200);         //Withdraw #11
-        System.out.println(account1);          //ToString #12
+        //account1.withdraw(200);              //Withdraw #11
+        //System.out.println(account1);          //ToString #12
+        account1.transfer(account2,100);
     }
 
     public static class BankAccount{
@@ -16,7 +18,28 @@ public class ex13 {
         double transactionFee = 0.00;
 
         public void deposit(double amount){
-            balance = balance + amount;
+            balance = balance + Math.abs(amount);
+        }
+
+        public void transfer(BankAccount account, double amount){
+            double tempbalance = this.balance;
+            System.out.println("You have " + balance + " on your account" );
+            System.out.println("Enter amout to transfer to " + account.name);
+            amount = input.nextDouble();
+            transactionFee = 5.00;
+
+            tempbalance = tempbalance - amount - transactionFee;
+            System.out.println(tempbalance + " temp balance");
+
+            if (tempbalance <= 0){
+                System.out.println("No Funds");
+            } else {
+                balance = tempbalance;
+                account.balance = account.balance + amount;
+                System.out.println("You transferd " + amount + " to " + account.name);
+                System.out.println("your new balance: " + "$" + balance);
+                System.out.println(account.name +" new balance: "+"$" +account.balance);
+            }
         }
 
 
@@ -34,7 +57,7 @@ public class ex13 {
             } else {
                 balance = tempbalance;
                 System.out.println("withdraw" + amount);
-                System.out.println("new balance" + balance);
+                System.out.println("new balance " + "$"+ balance);
             }
 
         }
